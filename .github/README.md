48e5869ccfe46a923ad8bed1ceb5814994c58907
aubique <ubicquita@gmail.com>
2020-06-30 11:47:00 +0200

    Retrieve datasets directly from data.gouv.fr

--- .github/README.md
+++ .github/README.md
@@ -4,6 +4,8 @@ The heat map is a graphical representation of COVID-19 case data to give a
 quick impression of the location of cases in every region of France.
 The map is daily updated.
 
+The datasets are retrieved from the [open platform for French public data](https://data.gouv.fr).
+
 <img src="./screenshot_1.jpg" height="400" />
 
 ## Tech Stack
@@ -11,7 +13,7 @@ The map is daily updated.
 This project is a single-page application (SPA) with interactive map.
 
 ### Front-End:
-- Angular 9
+- Angular 10
 - Bootstrap 4.5
 - FusionCharts map library
 - Font Awesome icons
--- covid-ui/src/app/core/http/api.service.ts
+++ covid-ui/src/app/core/http/api.service.ts
@@ -9,11 +9,14 @@ import { Observable } from 'rxjs';
 })
 export class ApiService {
 
+  public static readonly OPEN_COVID_GIT = 'https://raw.githubusercontent.com/opencovid19-fr/data/aa247f092c6cf27fced4b589f9b409292cac0456/data-sources/sante-publique-france/covid_hospit.csv';
+  public static readonly DIRECT_DATA_GOUV = 'https://www.data.gouv.fr/fr/datasets/r/63352e38-d353-4b54-bfd1-f1b3ee1cabd7';
+  public static readonly ROUTE53_DATA_GOUV = 'http://data.covid19map.fr';
+
   constructor(private http: HttpClient) {
   }
 
   public fetchOpencovidCsv(): Observable<string> {
-    const rawCsv = 'https://raw.githubusercontent.com/opencovid19-fr/data/master/data-sources/sante-publique-france/covid_hospit.csv';
-    return this.http.get(rawCsv, {responseType: 'text'});
+    return this.http.get(ApiService.DIRECT_DATA_GOUV, {responseType: 'text'});
   }
 }
